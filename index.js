9f1cbde7e0baa4640f0852aee6e0fb82fea19720
Mays Jones <maysjones2@gmail.com>
2022-03-16 22:43:39 -0400

    commit tree 90a1d14efa4c1356622062ffa17a567b84843ef8

---
+++ index.js
@@ -0,0 +1,13 @@
+function shout(hello){
+    return hello.toUpperCase();
+}
+function whisper(HELLO){
+    return HELLO.toLowerCase();
+}
+
+function logShout(string){
+    console.log( string.toUpperCase());
+}
+function logWhisper(STRING){
+    console.log(STRING.toLowerCase());
+}
